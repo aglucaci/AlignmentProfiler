a942a8d17708f5e5e57b56710b460bc2d2e83de5
Alex <alex@Alexs-MacBook-Pro.local>
2022-10-08 18:57:30 -0400

    added invariant site calculator

--- src/AlignmentProfiler/AlignmentProfiler.py
+++ src/AlignmentProfiler/AlignmentProfiler.py
@@ -53,6 +53,7 @@ import os
 import sys
 import pathlib 
 
+
 # =============================================================================
 # Declares
 # =============================================================================
@@ -64,6 +65,29 @@ parser.add_argument('--input', metavar='I', type=str,
 
 args = parser.parse_args()
 
+
+Universal_codon_table = {"Glycine": ["GGT", "GGC", "GGA", "GGG"], 
+                         "Alanine": ["GCT", "GCC", "GCA", "GCG"],
+                         "Valine": ["GTT", "GTC", "GTA", "GTG"],
+                         "Leucine": ["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"],
+                         "Methionine": ["ATG"],
+                         "Isoleucine":  ["ATT", "ATC", "ATA"],
+                         "Phenylalanine": ["TTT", "TTC"] ,
+                         "Tyrosine": ["TAT", "TAC"],
+                         "Tryptophan": ["TGG"],
+                         "Serine": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+                         "Threonine": ["ACT", "ACC", "ACA", "ACG"],
+                         "Cysteine": ["TGT", "TGC"],
+                         "Proline": ["CCT", "CCC", "CCA", "CCG"],
+                         "Asparagine": ["AAT", "AAC"],
+                         "Glutamine":  ["CAA", "CAG"], 
+                         "Lysine": ["AAA", "AAG"],
+                         "Arginine": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"], 
+                         "Histidine": ["CAT", "CAC"],
+                         "Aspartate": ["GAT", "GAC"], 
+                         "Glutamate": ["GAA", "GAG"],
+                         "Stop":  ["TAA", "TAG", "TGA"]}
+
 # =============================================================================
 # Classes
 # =============================================================================
@@ -72,6 +96,7 @@ class AlignmentProfiler(): # Loads in a fasta dictionary
   def __init__(self, _Alignment, MoleculeType="DNA"):
     
     File_Extension = pathlib.Path(_Alignment).suffix
+    
     if File_Extension == ".nex":
         File_Type = "nexus"
     else:
@@ -87,8 +112,10 @@ class AlignmentProfiler(): # Loads in a fasta dictionary
     self.MoleculeType = MoleculeType
     self.GapCharacter = "-"                    # Default
     self.DNA_Characters = ["T", "C", "G", "A"] # Default
-    
-    self.NumInvariantSites = Get_NumInvariantSites(_Alignment, File_Type)
+    # Statistics
+    self.NumInvariantSites = self.Get_NumInvariantSites(_Alignment, File_Type)
+    self.Gappiness = {}
+    self.NTFrequencies = {}
   #end method
 
   def Get_NumSites(self, _Alignment, File_Type):
@@ -117,26 +144,43 @@ class AlignmentProfiler(): # Loads in a fasta dictionary
       return sequences  
   #end method
   
-  """
   def Get_NumInvariantSites(self, _Alignment, File_Type):
       InvariantSites = 0
-      
-      for i in range(self.NumSites):
-          column_data = []
-          
-          with open(_Alignment, "r") as handle:
-              for record in SeqIO.parse(handle, File_Type):
-                  if len(str(record.seq)) % 3 == 0:
-                      sites = int(len(str(record.seq)) / 3)
-                      return sites
-                  else:
-                      print("# ERROR: Number of sites is not a multiple of 3")
-                #end if
-              #end for
-          #end with
-    
+      #VariableSites = 0
+      #loop over every site
+      #for i in range(0, self.NumSites):
+      print("# Checking alignment for invariant sites")
+      for i in tqdm(range(self.NumSites)):
+            #print("# Checking site:", i + 1)
+            column_data = []
+            Variable = False
+            with open(_Alignment, "r") as handle:
+                for record in SeqIO.parse(handle, File_Type):
+                    codon = record.seq[i * 3 : (i * 3) + 3]
+                    #print(i + 1, codon)
+                    if column_data == []:
+                        column_data.append(codon)
+                    else:
+                        # column data is not empty
+                        # check it
+                        if column_data[0] != codon:
+                            # Variable site
+                            # Go to next site
+                            #print("Variable site:", i+1)
+                            #VariableSites += 1
+                            Variable = True
+                            break
+                        #end if
+                    #end if
+                #end for
+            #end with
+            if Variable == False:
+                InvariantSites += 1
+            #end if
+        #end for
+      return InvariantSites
   #end method
-  """
+  
 
 
 #end class
@@ -148,21 +192,34 @@ class AlignmentProfiler(): # Loads in a fasta dictionary
 # =============================================================================
 # Main
 # =============================================================================
-
+print()
+print("#", "".join(["="]*78))
 print("# Starting to profile multiple sequence alignment:", args.input)
+print("#", "".join(["="]*78))
+print()
 
 if os.path.exists(args.input) and os.path.getsize(args.input) > 0:
     _MSA = AlignmentProfiler(args.input)
+    print()
+    print()
     print("# Loading complete on an alignment with", _MSA.NumSequences, "sequences, and", _MSA.NumSites, "sites")
+    print()
 else:
     print("# ERROR: the file is either empty or does not exist")
 #end if
 
+print("#", "".join(["="]*78))
+print("# Reporting alignment statistics")
+print("#", "".join(["="]*78))
+print()
+
 # Invariant Sites
+print("# The alignment has", _MSA.NumInvariantSites, "invariant sites", "(", (_MSA.NumInvariantSites / _MSA.NumSites) * 100, "% ) ")
 
+# Gaps
+print("# The alignment has an average gappiness of", 0, "with a range of", 0, "to", 0)
 
-
-
+# Nucleotide Frequencies
 
 """
 # Summary stats
